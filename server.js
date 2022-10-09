5b4c6896800b283841a59564c807e67a67c203d6
jncfintech <115144435+jncfintech@users.noreply.github.com>
2022-10-09 12:36:42 +0530

    change port

--- server.js
+++ server.js
@@ -26,314 +26,314 @@ const URI = process.env.URI;
 app.use(cors({ credentials: true, origin: 'https://zepverse.org/' }));
 app.use(express.json());
 
-const PORT =8000 || process.env.PORT
+const PORT = 80 || process.env.PORT
 
 
 mongoose.connect(URI);
 
 
 mongoose.connection.once('open', () => {
-    console.log('data base connestion is done');
-   
-  })
-  
+  console.log('data base connestion is done');
 
-  app.get('/', (req, res) => {
-    console.log('hi from the server');
-    res.json('hi from server');
+})
+
+
+app.get('/', (req, res) => {
+  console.log('hi from the server');
+  res.json('hi from server');
+});
+
+
+app.post('/fetchtweetidtaskresponse', async (req, res) => {
+  const { email } = req.body
+
+  const response = await tweetidtask.findOne({
+    email: email
+  });
+
+  res.json(response)
+})
+
+app.post('/fetchfollowtaskresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const followTaskresponse = await followtasks.findOne({
+    loggedUserData: loggedUserData
+  });
+
+  console.log('followTaskresponse')
+  console.log(followTaskresponse)
+
+  console.log('loggeduserdata')
+  console.log(loggedUserData)
+
+  res.json(followTaskresponse)
+
+
+})
+
+app.post('/fetchretweetTaskresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const retweetTaskresponse = await retweettasks.findOne({
+    loggedUserData: loggedUserData,
+  });
+
+  console.log('fetchretweetTaskresponse')
+  console.log(retweetTaskresponse)
+
+  res.json(retweetTaskresponse)
+
+
+})
+
+
+app.post('/fetchwalletAddressresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const walletAddressresponse = await walletaddresstasks.findOne({
+    loggedUserData: loggedUserData,
   });
 
 
-  app.post('/fetchtweetidtaskresponse',async (req,res)=>{
-    const {email} = req.body
 
-    const response = await tweetidtask.findOne({
-      email : email
+
+
+
+  res.json(walletAddressresponse)
+
+
+})
+
+
+app.post('/fetchtweettaskresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const tweetTaskresponse = await tweettasks.findOne({
+    loggedUserData: loggedUserData,
+  });
+
+
+
+
+
+
+  res.json(tweetTaskresponse)
+
+
+})
+
+app.post('/fetchtelegramtaskresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const telegramTaskresponse = await telegramtasks.findOne({
+    loggedUserData: loggedUserData,
+  });
+
+
+  res.json(telegramTaskresponse)
+
+
+})
+
+app.post('/fetchbuytaskresponse', async (req, res) => {
+  const { loggedUserData } = req.body;
+
+  const buyTaskresponse = await buytasks.findOne({
+    loggedUserData: loggedUserData,
+  });
+
+
+  res.json(buyTaskresponse)
+
+
+})
+
+app.post('/savebuystatus', async (req, res) => {
+
+  console.log('savetweettask')
+  console.log(req.body)
+
+  try {
+    const buytask = new buytasks({
+      loggedUserData: req.body.loggedUserData,
+      buy: true,
     });
+    const savedData = await buytask.save();
+    res.send(savedData);
 
-    res.json(response)
-  })
+  } catch (err) {
+    console.log(err);
 
-  app.post('/fetchfollowtaskresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
+  }
 
-    const followTaskresponse = await followtasks.findOne({
-      loggedUserData : loggedUserData
+
+})
+
+app.post('/savetweettaskstatus', async (req, res) => {
+
+  console.log('savetweettask')
+  console.log(req.body)
+
+  try {
+    const tweettask = new tweettasks({
+      loggedUserData: req.body.loggedUserData,
+      tweet: req.body.tweet,
     });
+    const savedData = await tweettask.save();
+    res.send(savedData);
 
-    console.log('followTaskresponse')
-    console.log(followTaskresponse)
+  } catch (err) {
+    console.log(err);
 
-    console.log('loggeduserdata')
-    console.log(loggedUserData)
+  }
 
-    res.json(followTaskresponse)
 
-   
-  })
+})
 
-  app.post('/fetchretweetTaskresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
 
-    const retweetTaskresponse = await retweettasks.findOne({
+app.post('/savetelegramtaskstatus', async (req, res) => {
+
+  const { joinTelegram, loggedUserData } = req.body;
+
+
+  try {
+    const telegram = new telegramtasks({
       loggedUserData: loggedUserData,
+      joinTelegram: joinTelegram,
     });
+    const savedData = await telegram.save();
+    res.send(savedData);
 
-   console.log('fetchretweetTaskresponse')
-   console.log(retweetTaskresponse)
+  } catch (err) {
+    console.log(err);
 
-    res.json(retweetTaskresponse)
-
-   
-  })
+  }
 
 
-  app.post('/fetchwalletAddressresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
+})
 
-    const walletAddressresponse = await walletaddresstasks.findOne({
+
+app.post('/savefollowtaskstatus', async (req, res) => {
+
+  const { twitterFollow, loggedUserData } = req.body;
+
+  try {
+    const follow = new followtasks({
       loggedUserData: loggedUserData,
+      twitterFollow: twitterFollow,
     });
+    const savedData = await follow.save();
+    res.send(savedData);
 
+  } catch (err) {
+    console.log(err);
 
-   
+  }
 
-    
+})
 
-    res.json(walletAddressresponse)
+app.post('/savewallettaskstatus', async (req, res) => {
 
-   
-  })
+  const { walletAddress, loggedUserData } = req.body;
 
-
-  app.post('/fetchtweettaskresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
-
-    const tweetTaskresponse = await tweettasks.findOne({
+  try {
+    const walletaddress = new walletaddresstasks({
       loggedUserData: loggedUserData,
+      walletAddress: walletAddress,
     });
+    const savedData = await walletaddress.save();
+    res.send(savedData);
+
+  } catch (err) {
+    console.log(err);
+
+  }
+
+})
 
 
-   
+app.post('/saveretweettaskstatus', async (req, res) => {
 
-    
+  const { retweet, loggedUserData } = req.body;
 
-    res.json(tweetTaskresponse)
+  console.log(retweet, loggedUserData);
 
-   
-  })
-
-  app.post('/fetchtelegramtaskresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
-
-    const telegramTaskresponse = await telegramtasks.findOne({
+  try {
+    const retwee = new retweettasks({
       loggedUserData: loggedUserData,
+      retweet: retweet,
     });
- 
+    const savedData = await retwee.save();
+    res.send(savedData);
 
-    res.json(telegramTaskresponse)
+  } catch (err) {
+    console.log(err);
 
-   
-  })
+  }
 
-  app.post('/fetchbuytaskresponse',async (req,res)=>{
-    const {loggedUserData} = req.body;
+})
 
-    const buyTaskresponse = await buytasks.findOne({
-      loggedUserData: loggedUserData,
+
+app.post('/savetweetid', async (req, res) => {
+
+  const { email, tweetid } = req.body;
+
+  console.log(tweetid);
+
+  try {
+    const tweetidsave = new tweetidtask({
+      email: email,
+      tweetid: tweetid,
     });
- 
+    const savedData = await tweetidsave.save();
+    res.send(savedData);
 
-    res.json(buyTaskresponse)
-
-   
-  })
-
-  app.post('/savebuystatus',async (req,res)=>{
- 
-    console.log('savetweettask')
-    console.log(req.body)
-  
-    try {
-      const buytask = new buytasks({
-        loggedUserData : req.body.loggedUserData,
-        buy : true,
-      });
-      const savedData = await buytask.save();
-      res.send(savedData);
-  
-  }catch (err) {
+  } catch (err) {
     console.log(err);
-  
-  }
-  
-  
-  })
-  
-  app.post('/savetweettaskstatus',async (req,res)=>{
-   
-    console.log('savetweettask')
-    console.log(req.body)
-  
-    try {
-      const tweettask = new tweettasks({
-        loggedUserData : req.body.loggedUserData,
-        tweet : req.body.tweet,
-      });
-      const savedData = await tweettask.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
-  }
-  
-  
-  })
 
-
-  app.post('/savetelegramtaskstatus',async (req,res)=>{
-
-    const {joinTelegram,loggedUserData} = req.body;
-   
-  
-    try {
-      const telegram = new telegramtasks({
-        loggedUserData : loggedUserData,
-        joinTelegram: joinTelegram,
-      });
-      const savedData = await telegram.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
-  }
-  
-   
-  })
-  
-  
-  app.post('/savefollowtaskstatus', async(req,res)=>{
-  
-    const {twitterFollow,loggedUserData} = req.body;
-  
-    try {
-      const follow = new followtasks({
-        loggedUserData : loggedUserData,
-        twitterFollow: twitterFollow,
-      });
-      const savedData = await follow.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
-  }
-  
-  })
-  
-  app.post('/savewallettaskstatus', async(req,res)=>{
-  
-    const {walletAddress,loggedUserData} = req.body;
-  
-    try {
-      const walletaddress = new walletaddresstasks({
-        loggedUserData : loggedUserData,
-        walletAddress: walletAddress,
-      });
-      const savedData = await walletaddress.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
-  }
-  
-  })
-  
-  
-  app.post('/saveretweettaskstatus', async(req,res)=>{
-  
-    const {retweet,loggedUserData} = req.body;
-  
-    console.log(retweet,loggedUserData);
-  
-    try {
-      const retwee = new retweettasks({
-        loggedUserData : loggedUserData,
-        retweet: retweet,
-      });
-      const savedData = await retwee.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
-  }
-  
-  })
-
-
-  app.post('/savetweetid',async (req,res)=>{
-
-    const {email,tweetid} = req.body;
-  
-    console.log(tweetid);
-  
-    try {
-      const tweetidsave = new tweetidtask({
-        email : email,
-        tweetid: tweetid,
-      });
-      const savedData = await tweetidsave.save();
-      res.send(savedData);
-  
-  }catch (err) {
-    console.log(err);
-  
   }
 
-  })
+})
 
 
 
-  app.post('/checkfollower',async (req,res)=>{
+app.post('/checkfollower', async (req, res) => {
 
-    const {checkFollower} = req.body;
-  
-    // console.log('check follower api request')
-    // console.log(checkFollower)
-      
-  
+  const { checkFollower } = req.body;
+
+  // console.log('check follower api request')
+  // console.log(checkFollower)
+
+
   const config = {
     method: 'get',
     url: 'https://api.twitter.com/1.1/friendships/show.json?source_screen_name=zepcoinofficial&target_screen_name=' + checkFollower,
     headers: {
       Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAKRDhQEAAAAA9oKKdd9st2Veb53S%2BybkGn2z%2BZ8%3DXnCvsEJ1ekX0BZeCYuWjbqOtsAqircfWCMB7KsjtEMZaxRDaN1',
       Cookie: 'guest_id=v1%3A166391365524337597'
-      }
+    }
   };
-  
+
   axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-    // console.log(response.data)
-  
-    // res.send(JSON.stringify(response.data))
-    // res.send({data : JSON.stringify(response.data) , success : 1})
-    res.json({ success : 1})
-  })
-  .catch(function (error) {
-    console.log('error of check follower api')
-    console.log(error);
-    res.json({success : 0})
-  });
-  
-  
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      // console.log(response.data)
+
+      // res.send(JSON.stringify(response.data))
+      // res.send({data : JSON.stringify(response.data) , success : 1})
+      res.json({ success: 1 })
     })
+    .catch(function (error) {
+      console.log('error of check follower api')
+      console.log(error);
+      res.json({ success: 0 })
+    });
 
 
-  app.listen(PORT, () => {
-    console.log('server is run on port 8000');
+})
+
+
+app.listen(PORT, () => {
+  console.log('server is run on port 8000');
 })
